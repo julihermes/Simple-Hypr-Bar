135f3f2af7cf09fd8110f3dc75cc1df4d671bbeb
Julihermes Melo <julihermes@hotmail.com>
2025-06-09 21:51:32 -0300

    Change to GTK4 + Vala

--- common/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-import { App } from "astal/gtk3";
-
-export function toggleWindow(name: string, app: any = App) {
-  app.toggle_window(name);
-}
--- widget/AppLauncher/Toggle.tsx
+++
@@ -1,5 +0,0 @@
-import { toggleWindow } from "../../common/lib/utils";
-
-export default function ToggleAppLauncher() {
-  return <button onClick={() => toggleWindow("AppLauncher")}>󰣇</button>;
-}
